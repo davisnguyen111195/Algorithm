29b2d98ed0732abfff316855a51e1c1df7e6b1c2
datnt <nguyendat111195@gmail.com>
2024-04-01 15:01:04 +0700

    Using Legendre

--- LyThuyetSo/LyThuyetSo13.cpp
+++ LyThuyetSo/LyThuyetSo13.cpp
@@ -1,4 +1,25 @@
 //Using Legendre 
+
+// Cho số tự nhiên N và số nguyên tố P. Nhiệm vụ của bạn là tìm số x lớn nhất để N! chia hết cho p^x. Ví dụ với N=7, p=3 thì x=2 là số lớn nhất để 7! Chia hết cho 3^2.
+
+// Input Format
+
+// Cặp số N, p được viết cách nhau một khoảng trống.
+
+// Constraints
+
+// 1≤N≤10^14; 2≤p≤5000
+
+// Output Format
+
+// Đưa ra kết quả trên một dòng
+
+// Sample Input 0
+
+// 7 3
+// Sample Output 0
+
+// 2
 #include <iostream>
 #include <cmath>
 using namespace std;
