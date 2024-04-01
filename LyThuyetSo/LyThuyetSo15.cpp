1aac312819f0fb7ed15bb3d1edb497f42e18bbbd
datnt <nguyendat111195@gmail.com>
2024-04-01 16:10:35 +0700

    Delete rac

---
+++ LyThuyetSo/LyThuyetSo15.cpp
@@ -0,0 +1,46 @@
+//[Hàm, Lý Thuyết Số]. Bài 15. Số Sphenic
+// Số nguyên dương N được gọi là số Sphenic nếu N được phân tích duy nhất dưới dạng tích của ba thừa số nguyên tố khác nhau. Ví dụ N=30 là số Sphenic vì 30 = 2×3×5; N = 60 không phải số Sphenic vì 60 = 2×2×3×5. Cho số tự nhiên N, nhiệm vụ của bạn là kiểm tra xem N có phải số Sphenic hay không?
+
+// Input Format
+
+// Một số nguyên dương N
+
+// Constraints
+
+// 1≤N≤10^18
+
+// Output Format
+
+// Đưa ra 1 hoặc 0 tương ứng với N là số Sphenic hoặc không.
+
+// Sample Input 0
+
+// 999923001838986077
+// Sample Output 0
+
+// 1
+// Sample Input 1
+
+// 30
+// Sample Output 1
+
+// 1
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+
+bool checkSphenic(ll n){
+
+    for(ll i = 2; i <= n; i++) {
+        while(n % i == 0) {
+            
+        }
+    }
+}
+
+
+int main(){
+    return 0;
+}
