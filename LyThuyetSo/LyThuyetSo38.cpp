0c88436b64041e9af3ea3d629e4003978c9cc616
vedan200 <nguyendat111195@gmail.com>
2024-04-03 22:01:35 +0700

    LOADING

--- LyThuyetSo/LyThuyetSo38.cpp
+++ LyThuyetSo/LyThuyetSo38.cpp
@@ -20,3 +20,30 @@
 
 // 8
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using ll = long long;
+
+int degree(ll n, int p){
+    int ans = 0;
+    for(auto i = p; i <= n; i+=p){
+        int tmp = i;
+        while(tmp % p == 0){
+            ans++;
+            tmp = tmp / i;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    ll n; cin >> n;
+    ll sum = 1;
+    ll counter = 0;
+
+    cout << degree(4, 2);
+    return 0;
+}
+
