bf1a53cbcfc8b7d93ba4ab5160afd3119e40f60a
datnt <nguyendat111195@gmail.com>
2024-04-05 16:55:33 +0700

    FIX

--- 4_DongDuFibonacy/Hambosung13.cpp
+++ 4_DongDuFibonacy/Hambosung13.cpp
@@ -24,31 +24,23 @@
 
 #define ll long long
 using namespace std;
+ll f[100];
 
-
-bool fiboChecker(ll n){
-    if(n == 0 || n == 1){
-        return true;
-    }
-    ll f[100];
+void fiboGenerator(void){
     f[0] = 0;
     f[1] = 1;
-
     for(int i = 2; i <= 92; i++){
         f[i] = f[i-1] + f[i-2];
-        if(n == f[i]){
-            return true;
-        }
     }
-    return false;
 }
 
 
 int main() {
     ll n; cin >> n;
-    for(ll i = n; i++;){
-        if(fiboChecker(i)){
-            cout << i;
+    fiboGenerator();
+    for(int i = 0; i <= 92; i++){
+        if(f[i] > n){
+            cout << f[i];
             return 0;
         }
     }
