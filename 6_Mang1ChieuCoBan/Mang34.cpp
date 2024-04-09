e956fca918ec6a1f8c012738c0a18b7b7b371a88
datnt <nguyendat111195@gmail.com>
2024-04-09 10:00:47 +0700

    DONE 34

--- 6_Mang1ChieuCoBan/Mang34.cpp
+++ 6_Mang1ChieuCoBan/Mang34.cpp
@@ -28,39 +28,48 @@
 #include <iostream>
 
 using namespace std;
-void deleleElement(int a[], int &n, int pos);
+//void deleleElement(int a[], int &n, int pos);
 int main(){
     int n; cin >> n; int a[n];
+    
     for(int i = 0; i < n; i++){
         cin >> a[i];
     }
-    int b[n];
+    int cntDel = 0;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(i != j){
-                if(a[i] == a[j]){
-                    a[j] = -1;
+        for(int j = i+1; j <= n-1; j++){
+            if(a[i] == a[j] && a[i] != -1){
+                a[j] = -1;
+                cntDel++;
+                if(a[j+1] != a[i]){
+                    break;
                 }
+            } else {
+                break;
             }
         }
-        b[i] = a[i];
     }
-    int tmp = n;
-    for(int i = 0; i < tmp; i++){
-        if(b[i] == -1){
-            deleleElement(a, n, i);
+    int b[n-cntDel];
+    int tmp = 0;
+    for(int i = 0; i < n; i++){
+        
+        if(a[i] != -1){
+            
+            b[tmp] = a[i];
+            cout << b[tmp] << " ";
+            tmp++;
+            
         }
+        
     }
 
-    for(int i = 0; i < n; i++){
-        cout << a[i] << " ";
-    }
+
     return 0;
 }
 
-void deleleElement(int a[], int &n, int pos){
-    for(int i = pos; i < n-1; i++){
-        a[i] = a[i+1];
-    }
-    n--;
-}
+// void deleleElement(int a[], int &n, int pos){
+//     for(int i = pos; i < n-1; i++){
+//         a[i] = a[i+1];
+//     }
+//     n--;
+// }
