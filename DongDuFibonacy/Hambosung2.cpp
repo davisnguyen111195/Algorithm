28e7cc30cc669ecc541d29058acdaf4851290910
vedan200 <nguyendat111195@gmail.com>
2024-04-14 11:25:55 +0700

    Merge

---
+++ DongDuFibonacy/Hambosung2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+#define ll long long
+
+using namespace std;
+vector<int> d(0);
+
+void ptsnt(ll n){
+    for(ll i = 2; i * i <= n; i++){
+        int counter = 0;
+        if(n % i == 0) {
+            while(n % i == 0){
+                counter++;
+                n /= i;
+            }
+            d[i] = counter;
+        }
+    }
+    if(n > 1){
+        d[n] = 1;
+    }
+}
+
+int main() {
+    ll n; cin >> n;
+    ptsnt(n);
+    int maxI = -1;
+    for(int i = 2; i <= 10; i++){
+        maxI = max(maxI, d[i]);
+    }
+    cout << maxI << endl;
+    return 0;
+}
