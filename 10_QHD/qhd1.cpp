679136482a1d432fcdf87f1cc89094cfae198d6d
datnt <nguyendat111195@gmail.com>
2024-04-17 12:11:04 +0700

    Hoc ly thuyet qhd

---
+++ 10_QHD/qhd1.cpp
@@ -0,0 +1,52 @@
+// [QHD Basic]. Bài 1. Giai Thừa Chia Dư
+// Đề bài rất đơn giản, bạn hãy tính N! chia dư cho (10^9 + 7).
+
+// Gợi ý : Xây dựng mảng F[], trong đó F[i] lưu i! chia dư cho 10^9 + 7, mỗi test nhập n thì chỉ cần in ra F[n].
+
+// Input Format
+
+// Dòng 1 là số bộ test T
+
+// T dòng tiếp theo mỗi dòng là 1 số nguyên không âm N
+
+// Constraints
+
+// 1<=T<=10000
+
+// 0<=N<=10^6
+
+// Output Format
+
+// Đưa ra kết quả của mỗi test trên 1 dòng
+
+// Sample Input 0
+
+// 5
+// 11
+// 6
+// 8
+// 10
+// 13
+// Sample Output 0
+
+// 39916800
+// 720
+// 40320
+// 3628800
+// 227020758
+
+
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll f[10007];
+
+void f(ll &f){
+    
+}
+
+int main(){
+
+    return 0;
+}
