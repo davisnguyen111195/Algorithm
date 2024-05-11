16da97018efe44ae7b826206ed5a96dd8338de16
DavisNguyen <nguyendat111195@gmail.com>
2024-05-11 16:29:08 +0700

    Create bai16.cpp

---
+++ 14_mang2chieu/bai16.cpp
@@ -0,0 +1,37 @@
+// [Mảng 2 Chiều]. Bài 16. Đếm đảo 1
+// Cho ma trận nhị phân gồm N hàng và M cột chỉ bao gồm các số 0 và 1. Hãy đếm số lượng miền các số 1 trong ma trận, các ô số 1 được coi là cùng miền nếu chúng có chung cạnh.
+
+// Ví dụ về 1 ma trận nhị phân với 6 miền :
+
+
+
+// Đầu vào
+// Dòng đầu tiên N và M. N dòng tiếp theo mỗi dòng gồm M phần tử.
+
+// Giới hạn
+// 1≤N,M≤50
+
+// Đầu ra
+// In ra số lượng miền số 1 trong ma trận.
+
+// Ví dụ :
+// Input 01
+// Copy
+// 4 7
+// 0 1 1 0 1 1 0 
+// 1 1 1 1 1 1 1 
+// 1 0 0 0 0 1 1 
+// 1 1 0 1 0 0 0
+// Output 01
+// Copy
+// 2
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
