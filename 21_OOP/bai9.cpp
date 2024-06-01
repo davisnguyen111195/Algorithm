8eb85bfbe88ffc665d0cb707c837bf908fc0dd2f
vedan200 <nguyendat111195@gmail.com>
2024-06-01 07:49:35 +0700

    Dang lam bai 9

--- 21_OOP/bai9.cpp
+++ 21_OOP/bai9.cpp
@@ -28,3 +28,78 @@
 // Copy
 // HP05 Nguyen Van Ha 5 9640000
 
+#include <bits/stdc++.h>
+
+using namespace std;
+class GV{
+private:
+    string ma;
+    string ten;
+    string hsl;
+public:
+    GV(string ma, string ten, string hsl){
+        this->ma = ma;
+        this->ten = ten;
+        this->hsl = hsl;
+    }
+};
+
+class SoLon{
+private: 
+    string so;
+public:
+    SoLon(string so){
+        this->so = so;
+    }
+    string csl(string b){
+        string a = this->so;
+        int du = 0;
+        int mid = 0;
+        string res = "";
+        a.insert(0, max(0, (int)(b.length() - a.length())), '0');
+        b.insert(0, max(0, (int)(a.length() - b.length())), '0');
+        for(int i = a.length()-1; i >= 0; i--){
+            mid = (int)(a[i]-48) + (int)(b[i]-48) + du;
+            du = mid / 10;
+            res += (char)(mid%10 + 48);
+        }
+        if(du > 0){
+            res = "1" + res;
+        }
+        return res;
+    }
+
+    string nsb(int b, int l){
+        string a = this->so;
+        string res = "";
+        int mid = 0;
+        int du = 0;
+        for(int i = a.length()-1; i >= 0; i--){
+            mid = ((int) a[i] - 48) * b + du;
+            du = mid / 10;
+            res = (char) (mid % 10 + 48) + res;
+        }
+
+        if(du > 0){
+            res = to_string(du) + res;
+        }
+        res.insert(res.length(), l, '0');
+        return res;
+    }
+
+    string nsl(string b){
+        string a = this->so;
+        string res = "";
+        for(int i = b.length()-1; i >= 0; i--){
+            string t = nsb(a, b[i]-48, b.length()-i-1);
+            res = csl(res, t);
+        }
+        return res;
+    }
+};
+
+int main(){
+    
+
+    return 0;
+}
